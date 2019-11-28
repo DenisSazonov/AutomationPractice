984b78f559fd11695871cac2aa46bede2456158e
Denis Sazonov <denis_sazonov@epam.com>
2019-11-28 14:32:03 +0300

    test

--- src/UI_Tests/BaseApp.py
+++ src/UI_Tests/BaseApp.py
@@ -15,7 +15,8 @@ class AutomationPractice:
 
     def find_element(self, locator, time=10):
         return WebDriverWait(self.driver, time). \
-            until(EC.presence_of_element_located(locator))
+            until(EC.presence_of_element_located(locator),
+                  message=f"Can't find element by locator {locator}")
 
     # def find_elements(self, locator, time=10):
     #     return WebDriverWait(self.driver, time). \
